9adcd4b2fad0b6052b7f1f9f7e5abd91c6ed54c5
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-03-28 12:15:27 -0300

    soma quadrados

--- PdLdP/atividade1.java
+++ PdLdP/atividade1.java
@@ -2,7 +2,7 @@ package PdLdP;
 
 public class atividade1 {
     public static void main(String[] args) {
-        System.out.println(somaSerieAlternada(4));
+        System.out.println(somaQuadrados(5));
     }
     public static double somaFracaoPotenciaDeDois(int n) {
         if (n == 0) return 0;
@@ -24,8 +24,18 @@ public class atividade1 {
     }
 
     public static int somaQuadrados(int n) {
+        if (n== 0) return 0;
         if (n == 1) return 1;
-        return n * n + somaQuadrados(n - 1);
+
+        int total = 1;
+        int contador = 3;
+
+        for (int i = 2; i <= n; i++){
+            total += contador;
+            contador += 2;
+        }
+
+        return total;
     }
             
 }

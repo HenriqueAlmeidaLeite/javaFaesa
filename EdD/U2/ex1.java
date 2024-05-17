fe64d85521e59b367d78d1ccc6998ac9d0564345
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-05-17 20:45:34 -0300

    pesquisar implementado

--- EdD/U2/ex1.java
+++ EdD/U2/ex1.java
@@ -11,63 +11,73 @@ public class ex1 {
 
     public static void menu(){
         LCItem lista = new LCItem();
+        boolean stop = true;
 
-        System.out.println();
-        System.out.println();
-        System.out.println("1 - Cadastrar");
-        System.out.println("2 - Pesquisa");
-        System.out.println("3 - Modificar o nome de um Item");
-        System.out.println("4 - Remove o último Item da lista");
-        System.out.println("5 - Remove um Item da lista");
-        System.out.println("6 - Imprimir na tela todos os Itens da lista");
-        System.out.println("7 - Terminar o programa");
-        System.out.println();
-        System.out.println("Digite o número do que voce quer fazer: ");
-
-        int opção = scanner.nextInt();
-        scanner.nextLine();
-        switch (opção) {
-            case 1:
-                int codigo = adicionarCódigo();
-                String nome = adicionarNome();
-                scanner.nextLine();
-                int posição = adicionarPosição();
-                Item item = new Item(codigo, nome);  
-                lista.insere(item, posição);
-                break;
-            
-            case 2:
-
-                break;
-
-            case 3:
-
-                break;
-
-            case 4:
-
-                break;
-
-            case 5:
-
-                break;
-
-            case 6:
-
-                break;
-
-            case 7:
+        while (stop == true) {
             System.out.println();
-            terminarPrograma();
-            scanner.close();
-                return;
+            System.out.println();
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Pesquisa");
+            System.out.println("3 - Modificar o nome de um Item");
+            System.out.println("4 - Remove o último Item da lista");
+            System.out.println("5 - Remove um Item da lista");
+            System.out.println("6 - Imprimir na tela todos os Itens da lista");
+            System.out.println("7 - Terminar o programa");
+            System.out.println();
+            System.out.println("Digite o número do que voce quer fazer: ");
 
-            default:
-            System.out.println("Opção inválida");
-            menu();
-                break;
+            int opção = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (opção) {
+                case 1:
+                    int codigo = adicionarCódigo();
+                    String nome = adicionarNome();
+                    scanner.nextLine();
+                    int posição = adicionarPosição();
+                    Item item = new Item(codigo, nome);  
+                    lista.insere(item, posição);
+                    break;
+                
+                case 2:
+                    System.out.println("Qual código você quer procurar?");
+                    int posi = lista.pesquisa(scanner.nextInt());
+                    if (posi == -1) {
+                        System.out.println("Não encontrei esse código");
+                        break;
+                    } else{
+                        System.out.println("O item que você digitou está na posição " + posi);
+                        break;
+                    }
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+
+                case 5:
+
+                    break;
+
+                case 6:
+
+                    break;
+
+                case 7:
+                System.out.println();
+                terminarPrograma();
+                scanner.close();
+                    return;
+
+                default:
+                System.out.println("Opção inválida");
+                menu();
+                    break;
+            }
         }
-
         
     }
 

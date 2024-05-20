f10df08c298c9370c22322669eec7a4a62c4c288
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-05-20 14:41:47 -0300

    com imports

---
+++ EdD/U2/NoItem.java
@@ -0,0 +1,27 @@
+package EdD.U2;
+
+
+public class NoItem {
+	private Item item;
+	private NoItem prox;
+	public NoItem(Item item) {
+		this.item = item;
+		this.prox = null;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public NoItem getProx() {
+		return prox;
+	}
+	public void setProx(NoItem prox) {
+		this.prox = prox;
+	}
+	@Override
+	public String toString() {
+		return "item=" + item ;
+	}
+}

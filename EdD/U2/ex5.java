593e142ceef568bbbd2b9336eb5709172a2c0fc5
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-05-21 09:38:25 -0300

    Create ex5.java

---
+++ EdD/U2/ex5.java
@@ -0,0 +1,51 @@
+package EdD.U2;
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LSEItem lista = new LSEItem();
+        int valor = 1;
+
+        while (valor != 0) {
+            System.out.println();
+            System.out.println("Digite o valor que você quer adicionar. Se não quiser adicionar nenhum, digite 0:");
+            valor = scanner.nextInt();
+            if (valor != 0) {
+                preencher(lista, valor);
+            }
+        }
+        int val = valor();
+        verificarMenoresQue(lista, val);
+        scanner.close();
+    }
+
+    public static void verificarMenoresQue(LSEItem lista, int valor) {
+        NoItem atual = lista.getPrim();  // Usando o método getPrim() para obter o primeiro nó
+        System.out.println();
+        System.out.println();
+        System.out.println("Os valores menores que " + valor + " são: ");
+        while (atual != null) {
+            if (atual.getItem().getCodigo() < valor) {
+                System.out.println(atual.getItem().getCodigo());
+            }
+            atual = atual.getProx();
+        }
+    }
+
+    public static int valor(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.println("Digite o valor que você quer verificar quais são menores: ");
+        int valor = scanner.nextInt();
+        scanner.close();
+        return valor;
+    }
+
+
+    public static void preencher(LSEItem lista, int valor){
+        Item item = new Item(valor, "");
+        lista.insereFinal(item);
+    }
+}

868d96ac0ee293abaa5dd3eaf4920dae074445cc
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-05-22 12:29:17 -0300

    terminei

--- EdD/U2/exercicios/PFex1.java
+++ EdD/U2/exercicios/PFex1.java
@@ -7,7 +7,9 @@ public class PFex1 {
     public static void main(String[] args) {
         PilhaItem pilha = new PilhaItem();
         lereEmpilhar(pilha);
-        removerMenoresQue10(pilha);
+        removerMaioresQue10(pilha);
+        System.out.println(pilha.toString());
+
     }
 
     public static void lereEmpilhar(PilhaItem pilha){
@@ -27,7 +29,18 @@ public class PFex1 {
         scanner.close();
     }
 
-    public static void removerMenoresQue10(PilhaItem pilha){
-        
+    public static void removerMaioresQue10(PilhaItem pilha) {
+        PilhaItem pilhaAuxiliar = new PilhaItem();
+
+        while (!pilha.isEmpty()) {
+            Item item = pilha.pop().getItem();
+            if (item.getCodigo() <= 10) {
+                pilhaAuxiliar.push(item);
+            }
+        }
+
+        while (!pilhaAuxiliar.isEmpty()) {
+            pilha.push(pilhaAuxiliar.pop().getItem());
+        }
     }
 }

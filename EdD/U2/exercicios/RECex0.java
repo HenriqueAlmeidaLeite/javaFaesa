3a2635d03134023b69b2584efbe11ce1c6678cc4
Henrique Almeida Leite <henriquealmeidaleite2005@gmail.com>
2024-05-22 19:06:59 -0300

    Update RECex0.java

--- EdD/U2/exercicios/RECex0.java
+++ EdD/U2/exercicios/RECex0.java
@@ -43,6 +43,7 @@ public class RECex0 {
         } else {
             System.out.println("O caracter que você digitou não está presente na String");
         }
+        scanner.close();
     }
 
     public static boolean ehOuNaoEh(String str, char ch){
